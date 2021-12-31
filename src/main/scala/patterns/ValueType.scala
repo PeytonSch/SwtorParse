3c5d3fbef14a013d9d8509684dd8885389f146ec
Peyton <peytonscherschel@mymail.mines.edu>
2021-12-30 17:00:07 -0700

    Comments!

--- src/main/scala/patterns/ValueType.scala
+++ src/main/scala/patterns/ValueType.scala
@@ -1,5 +1,12 @@
 package patterns
 
+
+/**
+ * This is the ValueType trait. It is used by the Parser. Patterns that contain values should extend this
+ * It will be used to define methods for extracting data and updating the GUI.
+ *
+ * This is very much a WIP
+ */
 trait ValueType {
   def getResult(): String
   def getResultType(): String
