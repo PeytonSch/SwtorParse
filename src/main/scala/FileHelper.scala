e38138870f89858f13d46b5abcd8aa4a5e8a01c5
Jerry <jerrywbirnbaum@gmail.com>
2022-01-01 11:20:15 -0800

    Starting working on combat log selector with uggo buttons

---
+++ src/main/scala/FileHelper.scala
@@ -0,0 +1,16 @@
+import java.io.File
+
+class FileHelper{
+    
+}
+
+object FileHelper{
+    def getListOfFiles(dir: String):List[File] = {
+      val d = new File(dir)
+      if (d.exists && d.isDirectory) {
+        d.listFiles.filter(_.isFile).toList
+      } else {
+        List[File]()
+      }
+    }
+}
