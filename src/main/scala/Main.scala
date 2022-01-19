07e351f3c721a66db3a4f5d0c76e4e8c7ddb5fce
Peyton <peytonscherschel@mymail.mines.edu>
2022-01-18 17:06:48 -0700

    Large refactoring introducing element loader

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,5 @@
 import Controller.Controller
+import UI.{ElementLoader, GuiTiles}
 import com.typesafe.config.ConfigFactory
 import eu.hansolo.tilesfx.Tile
 import parser.Parser
@@ -15,22 +16,12 @@ import scalafx.event.ActionEvent
 import scalafx.Includes._
 
 import java.io.File
-import java.nio.file.Paths
-import java.nio.file.Files
-import java.time.Instant
 import eu.hansolo.tilesfx.chart.ChartData
 import eu.hansolo.tilesfx.skins.LeaderBoardItem
 import eu.hansolo.tilesfx.tools.TreeNode
 import javafx.event.{Event, EventHandler}
-import javafx.fxml.FXMLLoader
 import parsing.Actors.Player
-import parsing.Result.ApplyEffect
-import patterns.Actions.SafeLogin
-import patterns.LogInformation
-import patterns.Result.{EnterCombat, ExitCombat}
-import scalafx.scene.input.MouseEvent
 
-import scala.collection.IterableOnce.iterableOnceExtensionMethods
 import java.util.prefs.{Preferences, PreferencesFactory}
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
@@ -50,8 +41,8 @@ object Main extends JFXApp3 {
   override def start(): Unit = {
 
     val controller = new Controller()
+    val elementLoader = new ElementLoader()
 
-    val config = ConfigFactory.load()
 
     //Initialize Java Preferences object
     val prefs: Preferences = Preferences.userNodeForPackage(this.getClass())
@@ -69,16 +60,13 @@ object Main extends JFXApp3 {
 
     val files = FileHelper.getListOfFiles("./SampleLogs")
 
-    // Tiles is all of the tiles in the UI. Contained and managed in a GuiTiles class
+    // Tiles is all of the tiles in the UI. Contained and managed in a UI.GuiTiles class
     val tiles : GuiTiles = new GuiTiles()
 
     // Set the last Timer Call to the current system time. This is a var so it can be updated. It controls the UI
     // Refresh rate, checking the time against the last time and the execution rate.
     var lastTimerCall = System.nanoTime()
     val program_execution_rate : Long = 200_000_000L
-
-    // This can be used to generate random numbers
-    val random = scala.util.Random
 
 
     // This parser class is used to pass logs. This is more in here as a test and not fully implemented.
@@ -96,7 +84,6 @@ object Main extends JFXApp3 {
       if (now > lastTimerCall + program_execution_rate) {
         lastTimerCall = now
 
-
         /**
          * This returns all lines from the log that are new this tick.
          * It returns them as instances of LogInformation
@@ -106,46 +93,7 @@ object Main extends JFXApp3 {
         // if there are new lines to parse
         if (result.size != 0) controller.parseLatest(result)
 
-
-
-
-
-        /**
-         * All this code is essentially updating the UI with Mock Data
-         * */
-
-        /** Timer Ran Code */
-        if (tiles.statusTile.getLeftValue() > 1000) { tiles.statusTile.setLeftValue(0); }
-        if (tiles.statusTile.getMiddleValue() > 1000) { tiles.statusTile.setMiddleValue(0); }
-        if (tiles.statusTile.getRightValue() > 1000) { tiles.statusTile.setRightValue(0); }
-        tiles.statusTile.setLeftValue(tiles.statusTile.getLeftValue() + random.nextInt(4))
-        tiles.statusTile.setMiddleValue(tiles.statusTile.getMiddleValue() + random.nextInt(3))
-        tiles.statusTile.setRightValue(tiles.statusTile.getRightValue() + random.nextInt(3))
-
-//        tiles.leaderBoardTile.getLeaderBoardItems().get(random.nextInt(3)).setValue(random.nextDouble() * 80)
-        //tiles.timelineTile.addChartData(new ChartData("", random.nextDouble() * 300 + 50, Instant.now()));
-        //tiles.timelineTile.calcAutoScale()
-        // if the current combat is not null, set to show player damage
-        if (controller.getCurrentCombat() != null) {
-          // TODO: This needs to have a static graph if the combat is complete
-          //tiles.timelineTile.addChartData(new ChartData(controller.getCurrentPlayerDamage(),java.time.Instant.now()))
-        }
-        //tiles.timelineTile.setMaxTimePeriod(java.time.Duration.ofSeconds(900))
-
-        /** Radar Percentiles Chart */
-        tiles.chartData1.setValue(random.nextDouble() * 50)
-        tiles.chartData2.setValue(random.nextDouble() * 50)
-        tiles.chartData3.setValue(random.nextDouble() * 50)
-        tiles.chartData4.setValue(random.nextDouble() * 50)
-        tiles.chartData5.setValue(random.nextDouble() * 50)
-        tiles.chartData6.setValue(random.nextDouble() * 50)
-        tiles.chartData7.setValue(random.nextDouble() * 50)
-        tiles.chartData8.setValue(random.nextDouble() * 50)
-
-        /** Right side bar chart for personal stats*/
-        //tiles.barChartTile.getBarChartItems().get(random.nextInt(8)).setValue(random.nextDouble() * 800);
-        //tiles.barChartTile.getBarChartItems().get(2).setValue(5000)
-
+        elementLoader.performTickUpdateLiveParsing(controller, tiles)
 
       }
     })
@@ -208,263 +156,12 @@ object Main extends JFXApp3 {
     val fileMenu = new Menu("Log Files")
     fileMenu.items = fileBuffer.toList
 
-    // TODO: Just about everything in here probably needs to be extracted out so that it can run in the main loop
-    val menuAction = (event: ActionEvent) => {
-      //println(s"You clicked ${event.getTarget.asInstanceOf[javafx.scene.control.MenuItem].getText}")
-
-      // set the current combat instance
-      controller
-        .setCurrentCombatInstance(controller.
-          getCombatInstanceById(event.getTarget.asInstanceOf[javafx.scene.control.MenuItem].getText))
-
-      /**
-       * Main DPS Chart
-       */
-
-      // clear out and add all of the combat instance data to the chart
-      val damageTimeSeries =  controller.getCurrentCombat().getPlayerInCombatActor().getDamageDoneTimeSeries()
-      val damagePerSecondTimeSeries = controller.getCurrentCombat().getPlayerInCombatActor().getDamagePerSecondTimeSeries()
-//      println(s"Current combat has a saved time series of ${damageTimeSeries.size} elements")
-      tiles.lineChartSeries.getData.removeAll()
-      tiles.barChartSeries.getData.removeAll()
-      tiles.barChartSeries.data = damageTimeSeries.toSeq.map(x => (x._1.toString(),x._2)).map(tiles.toCatagoryChartData)
-      tiles.lineChartSeries.data = damagePerSecondTimeSeries.toSeq.map(x => (x._1.toString(),x._2)).map(tiles.toCatagoryChartData)
-//      println(s"Got max value of ${damageTimeSeries.valuesIterator.max}")
-      tiles.yAxis.setUpperBound(damageTimeSeries.valuesIterator.max)
-
-      /**
-       * Helper function to get inner ring to add ability damage to.
-       * This is used for both damage taken from source and
-       * damage done from source ability.
-       * @param name
-       * @return
-       */
-      def getCorrectChild(name : String, from : String): TreeNode[ChartData] = {
-        if (from == "dtps") {
-          val root : java.util.List[TreeNode[ChartData]] = tiles.dtpstree.getAll
-          for (i <- 0 until root.size()){
-            if(root.get(i).getItem.getName == name) return root.get(i)
-          }
-        }
-        else {
-          val root : java.util.List[TreeNode[ChartData]] = tiles.damageDoneTree.getAll
-          for (i <- 0 until root.size()){
-            if(root.get(i).getItem.getName == name) return root.get(i)
-          }
-        }
-        // this is a backup, probably shouldn't happen
-        println("Error, returning root tree, this should not happen")
-        tiles.dtpstree
-      }
-
-      /**
-       * * * * * * Damage Done Section * * * * *
-       *
-       * Update the base types for the damage taken pie chart
-       */
-
-      /** TODO: This and the damage taken section have a lot of repeat code, can probably factor some of this
-       * out into functions. Both here and in the CombatActorInstance functions
-       */
-
-
-      // remove the all old data for both tiles
-      tiles.damageDoneTree.removeAllNodes()
-
-      // Colors by type from config
-      val internalColor: Color = if (config.hasPath("UI.Colors.DamageTypes.internal")){
-        tiles.colorLoader(config.getString("UI.Colors.DamageTypes.internal"))
-      } else {tiles.colorLoader("")}
-      val kineticColor: Color = if (config.hasPath("UI.Colors.DamageTypes.kinetic")){
-        tiles.colorLoader(config.getString("UI.Colors.DamageTypes.kinetic"))
-      } else {tiles.colorLoader("")}
-      val energyColor: Color = if (config.hasPath("UI.Colors.DamageTypes.energy")){
-        tiles.colorLoader(config.getString("UI.Colors.DamageTypes.energy"))
-      } else {tiles.colorLoader("")}
-      val elementalColor: Color = if (config.hasPath("UI.Colors.DamageTypes.elemental")){
-        tiles.colorLoader(config.getString("UI.Colors.DamageTypes.elemental"))
-      } else {tiles.colorLoader("")}
-      val regularColor: Color = if (config.hasPath("UI.Colors.DamageTypes.regular")){
-        tiles.colorLoader(config.getString("UI.Colors.DamageTypes.regular"))
-      } else {tiles.colorLoader("")}
-
-      for (types <- controller.getCurrentCombat().getPlayerInCombatActor().getDamageTypeDone()) {
-        // TODO: Need to make sure you have ALL the damage types here or they wont show
-        types._1 match {
-          case "internal" => {
-            new TreeNode(new ChartData("Internal", types._2, internalColor), tiles.damageDoneTree);
-          }
-          case "kinetic" => {
-            new TreeNode(new ChartData("Kinetic", types._2, kineticColor), tiles.damageDoneTree);
-          }
-          case "energy" => {
-            new TreeNode(new ChartData("Energy", types._2, energyColor), tiles.damageDoneTree);
-          }
-          case "elemental" => {
-            new TreeNode(new ChartData("Elemental", types._2, elementalColor), tiles.damageDoneTree);
-          }
-          case "No Type" =>
-          case x => {
-            println(s"Got Unknown Damage type: ${x}")
-            new TreeNode(new ChartData("Regular", types._2, regularColor), tiles.damageDoneTree);
-          }
-        }
-      }
-
-      /**
-       * Update the damage done from source tile ability data
-       */
-
-      for (types <- controller.getCurrentCombat().getPlayerInCombatActor().getDamageDoneStats()) {
-        types._1 match {
-          case "internal" => {
-            for (ability <- types._2) {
-              new TreeNode(new ChartData(ability._1, ability._2, internalColor), getCorrectChild("Internal","dps"));
-            }
-          }
-          case "kinetic" => {
-            for (ability <- types._2) {
-              new TreeNode(new ChartData(ability._1, ability._2, kineticColor), getCorrectChild("Kinetic","dps"));
-            }
-          }
-          case "energy" => {
-            for (ability <- types._2) {
-              new TreeNode(new ChartData(ability._1, ability._2, energyColor), getCorrectChild("Energy","dps"));
-            }
-          }
-          case "elemental" => {
-            for (ability <- types._2) {
-              new TreeNode(new ChartData(ability._1, ability._2, elementalColor), getCorrectChild("Elemental","dps"));
-            }
-          }
-          case "No Type" =>
-          case x => {
-            for (ability <- types._2) {
-              new TreeNode(new ChartData(ability._1, ability._2, regularColor), getCorrectChild("Regular","dps"));
-            }
-          }
-
-        }
-      }
-
-
-      /**
-       * * * * * * Damage Taken Section * * * * *
-       *
-       * Update the damage types indicator as well as the base types for the damage taken pie chart
-       */
-
-      // remove the all old data for both tiles
-      tiles.dtpstree.removeAllNodes()
-      tiles.damageFromTypeIndicator.clearChartData()
-
-      for (types <- controller.getCurrentCombat().getPlayerInCombatActor().getDamageTypeTaken()) {
-        // TODO: Need to make sure you have ALL the damage types here or they wont show
-        types._1 match {
-          case "internal" => {
-            tiles.damageFromTypeIndicator.addChartData(new ChartData("Internal",types._2,internalColor))
-            new TreeNode(new ChartData("Internal", types._2, internalColor), tiles.dtpstree);
-          }
-          case "kinetic" => {
-            tiles.damageFromTypeIndicator.addChartData(new ChartData("Kinetic",types._2,kineticColor))
-            new TreeNode(new ChartData("Kinetic", types._2, kineticColor), tiles.dtpstree);
-          }
-          case "energy" => {
-            tiles.damageFromTypeIndicator.addChartData(new ChartData("Energy",types._2,energyColor))
-            new TreeNode(new ChartData("Energy", types._2, energyColor), tiles.dtpstree);
-          }
-          case "elemental" => {
-            tiles.damageFromTypeIndicator.addChartData(new ChartData("Elemental",types._2,elementalColor))
-            new TreeNode(new ChartData("Elemental", types._2, elementalColor), tiles.dtpstree);
-          }
-          case "No Type" =>
-          case x => {
-            println(s"Got Unknown Damage type: ${x}")
-            tiles.damageFromTypeIndicator.addChartData(new ChartData("Regular",types._2,regularColor))
-            new TreeNode(new ChartData("Regular", types._2, regularColor), tiles.dtpstree);
-          }
-        }
-      }
-
-      /**
-       * Update the damage taken from source tile ability data
-       */
-
-      for (types <- controller.getCurrentCombat().getPlayerInCombatActor().getDamageTakenStats()) {
-        types._1 match {
-          case "internal" => {
-            for (ability <- types._2) {
-              new TreeNode(new ChartData(ability._1, ability._2, internalColor), getCorrectChild("Internal","dtps"));
-            }
-          }
-          case "kinetic" => {
-            for (ability <- types._2) {
-              new TreeNode(new ChartData(ability._1, ability._2, kineticColor), getCorrectChild("Kinetic","dtps"));
-            }
-          }
-          case "energy" => {
-            for (ability <- types._2) {
-              new TreeNode(new ChartData(ability._1, ability._2, energyColor), getCorrectChild("Energy","dtps"));
-            }
-          }
-          case "elemental" => {
-            for (ability <- types._2) {
-              new TreeNode(new ChartData(ability._1, ability._2, elementalColor), getCorrectChild("Elemental","dtps"));
-            }
-          }
-          case "No Type" =>
-          case x => {
-            for (ability <- types._2) {
-              new TreeNode(new ChartData(ability._1, ability._2, regularColor), getCorrectChild("Regular","dtps"));
-            }
-          }
-
-        }
-      }
-
-      /**
-       * Update the leaderboard tile with all player damage
-       *
-       *  we do not clear leaderboard items, and create new items, instead we iterate through them setting new values
-       * there are 24 by default, for the max swtor group size. Only set the number visible that
-       * correspond to the number of players in this combat. We have to do it this way because of something
-       * with how the leaderboard tile works
-       */
-
-      for (index <- 0 until tiles.leaderBoardItems.size()){
-        tiles.leaderBoardItems.get(index).setValue(0)
-        tiles.leaderBoardItems.get(index).setName("")
-        tiles.leaderBoardItems.get(index).setVisible(false)
-      }
-
-      // get all the combat Actors
-      val combatActors = controller.getCurrentCombat().getCombatActors()
-      val players = (for (actor <- combatActors) yield actor.getActor()).filter(_.isInstanceOf[Player])
-      var lastUpdatedIndex = 0
-      for (index <- 0 until players.length){
-        // need to relate the player actor instance to the combat actor instance
-        // then we need to get the damage done and order them
-        val combatInstanceActor = controller.getCurrentCombat().getCombatActorByIdString(players(index).getId().toString)
-        // TODO: I cannot get this to set the name to save my life, help!
-        tiles.leaderBoardItems.get(index).setValue(combatInstanceActor.getDamageDone())
-        tiles.leaderBoardItems.get(index).setName(combatInstanceActor.getActor().getName())
-        tiles.leaderBoardItems.get(index).getChartData.setName(combatInstanceActor.getActor().getName())
-        tiles.leaderBoardItems.get(index).setVisible(true)
-      }
-
-
-
-
-
-
-    }
-
     val combatInstanceMenu = new Menu("Combat Instances")
     var combatInstanceBuffer = new ListBuffer[MenuItem]()
     for (combatInstance <- controller.getAllCombatInstances()){
 //      println(s"Got combat instance: ${combatInstance}")
       var item = new MenuItem(combatInstance.getNameFromActors)
-      item.setOnAction(menuAction)
+      item.setOnAction(elementLoader.combatInstanceChangeMenuAction(controller, tiles))
       combatInstanceBuffer += item
     }
     combatInstanceMenu.items = combatInstanceBuffer.toList
